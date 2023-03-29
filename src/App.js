def886e9b03441660a06bfd35dfeca9bf1ccfaf9
shellby.miller <shellby.miller@gmail.com>
2023-03-29 18:59:02 -0400

    added google identity services login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 import './App.css';
 
 function App() {
+  const [ user, setUser ] = useState({});
+
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+  
+  function handleSignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+// useEffect hook empty array only runs this hook once
+useEffect(()=> {
+  /* global google */
+  google.accounts.id.initialize({
+    client_id: "166936624430-1nr0vphagnjt07c4mmbcg4cnjmeiro10.apps.googleusercontent.com",
+    callback: handleCallbackResponse
+
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    { theme: "outline", size: "large"}
+  );
+
+  google.accounts.id.prompt();
+},[]);
+// If we have no user: show sign in button
+// If we have a user: show logout button
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      { Object.keys(user).length !== 0 &&
+      <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+      }
+
+      { user &&
+      <div>
+        <h3>{user.name}</h3>
+        </div>
+        }
     </div>
   );
 }
