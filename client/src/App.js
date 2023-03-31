1190658cc0e0142e06405a50fa0b88d16b7a3463
shellby.miller <shellby.miller@gmail.com>
2023-03-31 15:52:30 -0400

    edits to routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,19 @@ import React from 'react';
 // Core style
 import './App.css';
 
-import { BrowserRouter } from 'react-router-dom'
+// Router
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+// Components
 import Navigation from './components/Navigation';
-import Home from './pages/Home/Home';
 import Footer from './components/Footer';
+
+
+// Pages
+import Home from './pages/Home/Home';
+import About from "./pages/About/About"
+import Gallery from "./pages/Gallery/Gallery"
+import Login from "./pages/Login/Login"
 
 
 
@@ -15,15 +23,30 @@ export default function App() {
 
   return (
     <BrowserRouter>
-    <div>
-      <Navigation />
-    </div>
-    <div>
-      <Home />
-    </div>
-    <div>
-      <Footer />
-    </div>
+
+      <div>
+        <Navigation />
+      </div>
+      
+    <Routes>
+        <Route path='/' element={<Home />}>
+        </Route>
+
+        <Route path="/about" element={<About />}>
+        </Route>
+
+        <Route path="/gallery" element={ <Gallery />}>
+        </Route>
+
+        <Route path="/login" element={<Login />}>
+        </Route>
+
+    </Routes>
+
+      <div>
+        <Footer />
+      </div>
+
     </BrowserRouter>
 
   );
--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home/Home";
-import About from "./About/About";
-import Gallery from "./Gallery/Gallery";
-import Login from "./Login/Login";
-
-export { Home, About, Gallery, Login };
