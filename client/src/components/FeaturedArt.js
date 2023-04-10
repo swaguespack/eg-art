8ba6434d2cc00bb27ce034c54bbf522ed6374fe6
mikafeng <mikafengler@gmail.com>
2023-04-10 16:16:45 -0400

    change import and structure of art into gallery

--- client/src/components/FeaturedArt.js
+++ client/src/components/FeaturedArt.js
@@ -1,37 +1,69 @@
 import React from "react";
 
 // import { waterColorA }  from "../imgs/index";
-import { arts } from "../imgs/gallery.js";
+// import { arts } from "../imgs/gallery.js";
+import {arts} from "../utils/artData"
 
 // empty array for images
-const images = [];
+// const images = [];
 
-//to loop through arts and push to images array
-for (const art in arts) {
-    images.push(<img
-        key={art}
-        className='arts'
-        alt={art}
-        src={arts[art].image}
-        aria-label={art}
-        // role='button'
-        // onClick={displaySomething}
-    />)
+export default function FeaturedArt(){
+    return (
+        <section id="gallery" className="container gallery-container">
+            <div className="row">
+                {arts.map((art) =>(
+                    <div className="col">
+
+                        <div className="card artCard">
+                            <img src={art.image} alt={art.alt} className=""/>
+
+                        </div>
+
+                    </div>
+                ))}
+
+            </div>
+
+
+        </section>
+    )
 }
 
-//JSX to be rendered
-const FeaturedArt = () => {
-   return ( 
-    <section className="galleryArt">
-     
-    <div className="container">
-        <h2>Featured Art</h2>
-        <div className="galleryImages">
-             <div className='arts'>{images}</div>
-        </div>
-    </div> 
-    </section>
-   );
-};
 
-export default FeaturedArt;
+
+
+
+
+
+
+
+
+//to loop through arts and push to images array
+// for (const art in arts) {
+//     images.push(<img
+//         key={art}
+//         className='arts'
+//         alt={art}
+//         src={arts[art].image}
+//         aria-label={art}
+//         // role='button'
+//         // onClick={displaySomething}
+//     />)
+// }
+
+// //JSX to be rendered
+// const FeaturedArt = () => {
+//    return ( 
+//     <section className="galleryArt">
+     
+//     <div className="container">
+//         <h2>Featured Art</h2>
+//         <div className="galleryImages">
+//              <div className='card arts'>{images}</div>
+//         </div>
+//     </div> 
+//     </section>
+//    );
+// };
+
+// export default FeaturedArt;
---
+++ client/src/utils/artData.js
@@ -0,0 +1,55 @@
+import abstract1 from '../imgs/gallery/gallery-abstract2.png'
+
+
+export const arts = [
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+    {
+        title: "title",
+        alt: "",
+        description: "description",
+        link: "",
+        image: abstract1
+    },
+
+]
