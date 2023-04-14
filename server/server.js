2e99fb02a12bca3921af5072efa96a9e97a8f3ca
shellby.miller <shellby.miller@gmail.com>
2023-04-13 21:28:03 -0400

    updated build

--- server/server.js
+++ server/server.js
@@ -23,6 +23,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startServer = async (typeDefs, resolvers) => {
