b5322bf896915f1ed0bbe30df80f35bc6f4da737
mikafeng <mikafengler@gmail.com>
2023-04-15 23:43:08 -0400

    adjust styling properties after importing bootstrap min

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import {Contact} from '../../components/index'
 // Style
+import {Container} from 'react-bootstrap';
 import "../../styles/pages/about.css"
 
 const About = () => {
     return (
-        <div className = "about-page">
-            <h1>About Page.</h1>
-            <Contact />
+        <Container>
+        <div className= "d-flex p-2 justify-content-around align-items-center">
+            <div className='flex-row mb-3'>
+                <div className='p-2'>
+                <h1>Contact</h1>
+                <Contact />
+                </div>
+                <div className='p-2 bg-primary'>
+                    <h1>About Page</h1>
+                </div>
+            </div>
         </div>
+        </Container>
     )
 }
 export default About
