41a8eea5911a85268ba65db374297c4708bdf144
mikafeng <mikafengler@gmail.com>
2023-04-17 15:35:39 -0400

    add homepage photo get rid of carosol

--- client/src/components/Carosol.js
+++ client/src/components/Carosol.js
@@ -1,30 +1,13 @@
 import React from "react";
 
+import abstract1 from "../assets/artImgs/abstract1.png";
+
 const Slides = () => {
 
     return (
 <>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="..."/>
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..."/>
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..."/>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+<img src={abstract1}/>
+
 </>
     )
 }
