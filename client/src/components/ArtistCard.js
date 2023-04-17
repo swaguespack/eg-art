578b11ae10cc64b900fd4442dc9bd4e36816019d
mikafeng <mikafengler@gmail.com>
2023-04-17 01:53:50 -0400

    change ArtistCard to use GraphQL and queryart

--- client/src/components/ArtistCard.js
+++ client/src/components/ArtistCard.js
@@ -1,30 +1,38 @@
-import React from "react"
-import {Card} from "react-bootstrap"
-import { ARTS } from "../assets/artArray"
+import React from "react";
+import { useQuery } from "@apollo/client";
+import {Card} from "react-bootstrap";
+// import { ARTS } from "../assets/artArray"
 
+import { QUERY_ART } from '../utils/queries';
 
-export default function ArtistCard () {
+const ArtistCard = () => {
     //option to filter artist by their medium
-const photographer = ARTS.filter(art =>
-    art.medium === 'photography');
+// const photographer = ARTS.filter(art =>
+//     art.medium === 'photography');
+    const {loading, data} = useQuery(QUERY_ART);
+    const artData = data?.arts || [];
 
+    if (loading) {
+        return <h3>Loading...</h3>
+    }
 
-const renderArtistCard = photographer.map((art, index) =>   
-        <Card id="artist-card" key={art.id} className="pt-5 m-3">
-            <Card.Title>{art.artist}</Card.Title>
-            <img
-            className="avatar"
-            src={art.avatar}
-            alt={art.alt}
-            />
-            <Card.Body>
-                <Card.Text>{art.description}{art.medium}</Card.Text>
-            </Card.Body>
-        </Card>
-        );
-
-
-    return (   
-        <>{renderArtistCard}</>
+    return(
+    <section>
+        {artData.map((art) =>   (
+            <Card id="artist-card" key={art.id} className="pt-5 m-3">
+                <Card.Title>{art.artist}</Card.Title>
+                <img
+                className="avatar"
+                src={art.avatar}
+                alt={art.alt}
+                />
+                <Card.Body>
+                    <Card.Text>{art.description}{art.medium}</Card.Text>
+                </Card.Body>
+            </Card>
+        ))}
+    </section>  
     );
 };
+
+export default ArtistCard;
